ea2951db5e4f74969b9840975c371ea4a6711cf0
Aichurek Zhalilova <anadan20182019@gmail.com>
2019-06-26 03:27:41 +0000

    place details and page 25

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,9 +1,10 @@
 class PlacesController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :show, :destroy, :edit, :update]
 
   def index
     @places=Place.all
   end
+
 
   def new
     @place=Place.new
@@ -17,6 +18,23 @@ class PlacesController < ApplicationController
   def show
     @place=Place.find(params[:id])
   end
+
+  def edit
+    @place = Place.find(params[:id])
+  end
+
+  def update
+      @place = Place.find(params[:id])
+      @place.update_attributes(place_params)
+      redirect_to root_path
+  end
+
+  def destroy
+   @place= Place.find(params[:id])
+   @place.destroy
+   redirect_to root_path
+  end
+
 
     private 
 
