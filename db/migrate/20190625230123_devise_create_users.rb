d5b8ce6dbeefff41db32847dee8d65e31a9c586f
Aichurek Zhalilova <anadan20182019@gmail.com>
2019-06-27 22:21:04 +0000

    db/sch

--- db/migrate/20190625230123_devise_create_users.rb
+++ db/migrate/20190625230123_devise_create_users.rb
@@ -1,18 +1,24 @@
 # frozen_string_literal: true
 
 class DeviseCreateUsers < ActiveRecord::Migration[5.2]
-  def change
-    create_table :users do |t|
+  
+    
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
 
+    #def 
+     # create_table :users do |t|
+      #t.string :email,              null: false, default: ""
+      #t.string :encrypted_password, null: false, default: ""
+      #t.string   :reset_password_token
+      #t.datetime :reset_password_sent_at
+      #t.datetime :remember_created_at
+      #t.timestamps null: false
+    #end
       ## Recoverable
-      t.string   :reset_password_token
-      t.datetime :reset_password_sent_at
+      
 
       ## Rememberable
-      t.datetime :remember_created_at
+     
 
       ## Trackable
       # t.integer  :sign_in_count, default: 0, null: false
@@ -33,8 +39,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       # t.datetime :locked_at
 
 
-      t.timestamps null: false
-    end
+  
 
     add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
